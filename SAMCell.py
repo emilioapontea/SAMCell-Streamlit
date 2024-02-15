fbc2afceb9f3a01caf16e9163eb5b8a6fa11e03f
emilioapontea <emilioapontea@gmail.com>
2024-02-15 16:43:36 -0500

    data visualization starting to work!!!!

--- SAMCell.py
+++ SAMCell.py
@@ -1,35 +1,40 @@
-import os
 import streamlit as st
-import csv
+from streamlit_image_comparison import image_comparison
+import pandas as pd
+import numpy as np
+from PIL import Image
 
-with st.sidebar:
-    "Welcome to the sidebar"
+@st.cache_data
+def df_to_csv(df):
+    return df.to_csv().encode('utf-8')
 
 st.title("SAMCell")
 st.caption("A Cell Segmentation Model powered by Segment Anything Model")
 
 uploaded_file = st.file_uploader("Upload an article", type=('.png', '.jpg', '.jpeg', 'tif', 'tiff'))
 
+
 if uploaded_file:
-    st.sucess(f"Uploaded {uploaded_file.name} successfully.")
+    image_comparison(
+        img1=Image.open(uploaded_file),
+        img2=Image.open(uploaded_file)
+    )
+    st.success(f"Uploaded {uploaded_file.name} successfully.")
 
-# Text input field for user to enter data
-# user_input = st.text_input("Enter some text:")
-lines_to_write = [
-    ['file name', 'cell count', 'avg cell area', 'confluency', 'avg neighbors'],
-]
+df = pd.DataFrame(columns=['file name', 'cell count', 'avg cell area', 'confluency', 'avg neighbors'])
 
-# Button to trigger writing data to file
-if st.button("Write to File"):
-    # Get the filename from the user
-    filename = st.text_input("Enter filename:", "metrics.csv")
-    os.makedirs("output", exist_ok=True)
+if st.sidebar.button("Generate random data"):
+    df.loc[len(df)] = (np.random.randint(0, 100, size=5))
+    st.dataframe(df)
 
-    # Open the file in write mode and write the user input
-    with open(os.path.join("output", filename), 'w', newline='') as csvfile:
-        # csvfile.write(user_input)
-        csv_writer = csv.writer(csvfile)
-        csv_writer.writerows(lines_to_write)
+csv = df_to_csv(df)
+file_name = "metrics.csv"
 
+if st.download_button(
+    label="Download analysis",
+    data = csv,
+    file_name=file_name,
+    mime='text/csv'
+):
     # Confirmation message
-    st.success(f"Data written to {filename} successfully.")
+    st.success(f"Data written to {file_name} successfully.")
