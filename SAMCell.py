1ae5f22c17396cecbe755adbc50c3607707f9857
emilioapontea <eaponte6@gatech.edu>
2024-02-22 18:31:24 -0500

    demo changes

--- SAMCell.py
+++ SAMCell.py
@@ -5,22 +5,51 @@ import numpy as np
 from PIL import Image
 from utils import *
 
+import requests
+from base64 import b64encode
+
 @st.cache_data
 def df_to_csv():
     return st.session_state['df'].to_csv().encode('utf-8')
 
 @st.cache_data
 def get_model_segmentation(uploaded_file, new_width):
-    model = ToyModel()
-    input_image = Image.open(uploaded_file)
-    output_image = model(np.array(input_image))
-    width_percent = (new_width / float(input_image.size[0]))
-    new_height = int((float(input_image.size[1]) * float(width_percent)))
-    return input_image.resize((new_width, new_height)), output_image
+    return Image.open(uploaded_file), Image.open('res/proc-orig.png')
+
+# @st.cache_data
+# def get_model_segmentation(uploaded_file, new_width):
+#     api_key = "SG_302ea47036b5e98f" #! DO NOT COMMIT
+#     url = "https://api.segmind.com/v1/sam-img2img"
+
+#     # Request payload
+#     data = {
+#     "image": b64encode(uploaded_file.read()).decode('utf-8')
+#     }
+
+#     print(f"Requesting SAM API on {uploaded_file.name}...")
+#     response = requests.post(url, json=data, headers={'x-api-key': api_key})
+#     if response.status_code == 200:
+#         # If the response contains JSON data, you can decode it
+#         json_response = response.json()
+#         # Now you can access specific fields in the JSON response
+#         # For example:
+#         print(json_response)
+#     else:
+#         # If the request was not successful, print the status code and reason
+#         print("Request failed with status code:", response.status_code)
+#         print("Reason:", response.reason)
+#         model = ToyModel()
+#         input_image = Image.open(uploaded_file)
+#         output_image = model(np.array(input_image))
+#         width_percent = (new_width / float(input_image.size[0]))
+#         new_height = int((float(input_image.size[1]) * float(width_percent)))
+
+#     return input_image.resize((new_width, new_height)), output_image
 
 @st.cache_data
-def append_metrics(file, output):
-    metrics = compute_metrics(output)
+def append_metrics(file, _output):
+    # metrics = compute_metrics(output)
+    metrics = [95, 1452.62, 20, 3.18]
     st.session_state['df'].loc[len(st.session_state['df'])] = (file.name, *metrics)
     st.session_state['imgs'][file.name] = file
     return metrics
