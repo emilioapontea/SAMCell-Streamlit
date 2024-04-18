21bf301a0503ab521bea3ce47eac779259371f5e
Emilio Aponte <44624188+emilioapontea@users.noreply.github.com>
2024-04-18 01:03:18 +0000

    UX fixes

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ if 'image_comparisons' not in st.session_state:
 if 'df' not in st.session_state:
     st.session_state['df'] = pd.DataFrame(columns=['file name', 'cell count', 'avg cell area', 'confluency', 'avg neighbors'])
 
-@st.cache_data
+@st.cache_data(show_spinner=False)
 def df_to_csv():
     return st.session_state['df'].to_csv().encode('utf-8')
 
-@st.cache_data
+@st.cache_data(show_spinner=False)
 def pil_to_png(img):
     buf = BytesIO()
     img.save(buf, format="PNG")
@@ -36,7 +36,7 @@ def init_query():
     response = requests.post(API_URL, headers=headers, json={})
     return response.json()
 
-@st.cache_data
+@st.cache_data(show_spinner=False)
 def query(payload):
     API_URL = st.secrets["db_url"]
     API_TOKEN = st.secrets["db_token"]
@@ -48,7 +48,58 @@ def query(payload):
     response = requests.post(API_URL, headers=headers, json=payload)
     return response.json()
 
-@st.cache_data
+@st.cache_data(show_spinner=False)
+def package_payload(uploaded_files):
+    # a list of ndarray
+    batch = load_images(uploaded_files)
+    # a list of base64 json dictionaries
+    images = []
+    for image in batch:
+        img_dict = {
+            'data': base64.b64encode(image.tobytes()).decode('utf-8'),
+            'shape': image.shape,
+            'dtype': str(image.dtype)
+        }
+        images.append(img_dict)
+
+    return {"inputs": images}
+
+@st.cache_data(show_spinner=False)
+def samcell_query(payload):
+    return query(payload)
+
+@st.cache_data(show_spinner=False)
+def get_model_outputs(uploaded_files, pred):
+    if 'labels' not in pred:
+        return [], {}
+
+    labels = pred["labels"]
+    # list of ndarray
+    outputs = []
+    for label in labels:
+        # Unpack response (base64 encoding of ndarray)
+        encoded_labels = label['data']
+        labels_shape = label['shape']
+        labels_dtype = label['dtype']
+        # Decode Base64 encoded data
+        decoded_response = base64.b64decode(encoded_labels)
+        # Reconstruct ndarray
+        output = np.frombuffer(decoded_response, dtype=np.dtype(labels_dtype))
+        output = output.reshape(labels_shape)
+
+        outputs.append(output)
+
+    #! This *MIGHT* mess up the order of names and outputs
+    rets = {}
+    for uploaded_file, output in zip(uploaded_files, outputs):
+        output_rgb = convert_label_to_rainbow(output)
+        # cv2.imwrite(f'/content/outputs/proc-orig.png', output_rgb)
+        ret = (Image.open(uploaded_file), Image.fromarray(output_rgb))
+        rets[uploaded_file.name] = ret
+
+    return outputs, rets
+
+@st.cache_data(show_spinner=False)
 def get_batch_segmentation(uploaded_files):
     # a list of ndarray
     batch = load_images(uploaded_files)
@@ -95,7 +146,8 @@ def get_batch_segmentation(uploaded_files):
 
     return outputs, rets
 
-@st.cache_data
+# DEPRECATED
+@st.cache_data(show_spinner=False)
 def get_model_segmentation(uploaded_file, new_width=1000):
     # return Image.open(uploaded_file), Image.open('res/proc-orig.png')
 
@@ -139,14 +191,19 @@ st.caption("A Cell Segmentation Model powered by Segment Anything Model  \nDevel
 info = st.empty()
 button = st.empty()
 if not st.session_state.endpoint_available:
-    if button.button('Get Started!'):
+    if button.button(
+        'Get Started!',
+        help='SAMCell may not currently be running. It may take a few minutes to initialize.',
+        type='primary',
+        use_container_width=True
+        ):
         button.empty()
         with st.spinner('Sit tight! SAMCell is starting up... (this may take a few minutes)'):
             q = None
             while q is None or q['error'] == '503 Service Unavailable':
                 info.info("SAMCell is setup to sleep after 15 minutes without requests", icon="🥱")
                 q = init_query()
-                time.sleep(5)
+                time.sleep(1)
             info.empty()
             st.session_state.endpoint_available = True
             st.rerun()
@@ -168,22 +225,37 @@ else:
             #     st.session_state.image_comparisons = {}
 
             # For batch endpoint requests (preferred)
-            outputs, st.session_state.image_comparisons = get_batch_segmentation(uploaded_files)
+            # outputs, st.session_state.image_comparisons = get_batch_segmentation(uploaded_files)
 
-            if len(st.session_state.image_comparisons.keys()) == 0:
-                st.error('Oh oh! SAMCell did not respond to your request! If the issue persists, contact GTPBL to restart the endpoint.', icon="😴")
-                st.session_state.image_comparisons = {}
-            else:
-                for file, output in zip(uploaded_files, outputs):
-                    metrics = compute_metrics(output)
+            with st.status("Sit tight! SAMCell is processing your images...", expanded=True) as status:
+                placeholder = st.empty()
+                container = placeholder.container()
+                container.write(":package: Packaging payload...")
+                payload = package_payload(uploaded_files)
+                container.write(":outbox_tray: Sending request...")
+                time.sleep(0.2)
+                container.write(":thought_balloon: Doing some linear algebra...")
+                pred = query(payload)
+                container.write(":magic_wand: Making some magic...")
+                outputs, st.session_state.image_comparisons = get_model_outputs(uploaded_files, pred)
 
-                    # print(f'image: {file.name}')
-                    # print(f'\t cell count: {cell_count}')
-                    # print(f'\t avg cell area: {cell_area}')
-                    # print(f'\t confluency: {confluency}')
-                    # print(f'\t avg neighbors: {avg_neighbors}')
+                if len(st.session_state.image_comparisons.keys()) == 0:
+                    status.update(label="SAMCell Error", state="error", expanded=True)
+                    placeholder.empty()
+                    st.error('Oh oh! SAMCell did not respond to your request! If the issue persists, contact GTPBL to restart the endpoint.', icon="😴")
+                    st.session_state.image_comparisons = {}
+                else:
+                    status.update(label="Segmentation complete!", state="complete", expanded=False)
+                    for file, output in zip(uploaded_files, outputs):
+                        metrics = compute_metrics(output)
 
-                    st.session_state['df'].loc[len(st.session_state['df'])] = (file.name, *metrics)
+                        # print(f'image: {file.name}')
+                        # print(f'\t cell count: {cell_count}')
+                        # print(f'\t avg cell area: {cell_area}')
+                        # print(f'\t confluency: {confluency}')
+                        # print(f'\t avg neighbors: {avg_neighbors}')
+
+                        st.session_state['df'].loc[len(st.session_state['df'])] = (file.name, *metrics)
 
 
     dropdown = st.selectbox(
